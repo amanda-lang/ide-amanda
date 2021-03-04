80ad6e6f5403ce748bc80e13ee2272930c206908
andersonkiole7 <torrinhoandersonkiole@gmail.com>
2021-03-04 00:25:38 +0000

    adicionar o projecto do linter

--- Linter-Amanda/Highlighting.cs
+++ Linter-Amanda/Highlighting.cs
@@ -1,11 +1,5 @@
-﻿using FastColoredTextBoxNS;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using System.Drawing;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Linter_Amanda
